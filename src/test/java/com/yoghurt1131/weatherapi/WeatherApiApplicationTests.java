28e95da704d34ec7f1d09dbef7e2a677a8d90f63
IshinFUKUOKA <yoghurt1131@gmail.com>
2018-12-30 18:16:08 +0900

    add initial spring boot test

--- src/test/java/com/yoghurt1131/weatherapi/WeatherApiApplicationTests.java
+++ src/test/java/com/yoghurt1131/weatherapi/WeatherApiApplicationTests.java
@@ -3,14 +3,20 @@ package com.yoghurt1131.weatherapi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ActiveProfiles("local")
+@SpringBootTest(classes = WeatherApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class WeatherApiApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testApplicationBoot() {
+		assertThat(true, is(true));
 	}
 
 }
