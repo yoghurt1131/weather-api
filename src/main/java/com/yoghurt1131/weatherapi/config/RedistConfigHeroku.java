e6cba61f9707cab961cc6f8dbe93fb8641c9682b
IshinFUKUOKA <yoghurt1131@gmail.com>
2019-08-10 15:06:57 +0900

    Setup JedisConnectionFactory Bean

--- src/main/java/com/yoghurt1131/weatherapi/config/RedistConfigHeroku.java
+++ src/main/java/com/yoghurt1131/weatherapi/config/RedistConfigHeroku.java
@@ -5,28 +5,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration.DefaultJedisClientConfigurationBuilder;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Profile("heroku")
 @Configuration
 public class RedistConfigHeroku {
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxIdle(5);
-        poolConfig.setMinIdle(1);
-        poolConfig.setTestOnBorrow(true);
-        poolConfig.setTestOnReturn(true);
-        poolConfig.setTestWhileIdle(true);
-        return  new JedisConnectionFactory(poolConfig);
+    public JedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
+        String envRedisUrl = System.getenv("REDIS_URL");
+        URI redisUri = new URI(envRedisUrl);
+
+        RedisStandaloneConfiguration hostConfig = new RedisStandaloneConfiguration();
+        hostConfig.setPort(redisUri.getPort());
+        hostConfig.setHostName(redisUri.getHost());
+        hostConfig.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+
+        JedisClientConfiguration.JedisClientConfigurationBuilder builder = JedisClientConfiguration.builder();
+        JedisClientConfiguration clientConfig = builder
+                .usePooling()
+                .build();
+
+        JedisConnectionFactory factory = new JedisConnectionFactory(hostConfig, clientConfig);
+        return factory;
     }
 
     @Bean
-    public RedisTemplate<String, City> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, City> redisTemplate(JedisConnectionFactory connectionFactory) {
         RedisTemplate<String, City> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
