d3d61cd5f51f83e93af827f2057bc0e4b2a8e98e
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-02-16 15:31:32 +0900

    move CurrentWeatherController to kotlin package

---
+++ src/main/kotlin/dev/yoghurt1131/weatherapi/controller/CurrentWeatherController.kt
@@ -0,0 +1,20 @@
+package dev.yoghurt1131.weatherapi.controller
+
+import dev.yoghurt1131.weatherapi.application.exception.ApiCallException
+import dev.yoghurt1131.weatherapi.application.service.WeatherApiService
+import dev.yoghurt1131.weatherapi.domain.CurrentWeather
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/current")
+@RestController
+class CurrentWeatherController(private val weatherApiService: WeatherApiService) {
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
+    @GetMapping("/city")
+    fun city(cityName: String) : CurrentWeather = weatherApiService.getCurrentWeather(cityName);
+}
