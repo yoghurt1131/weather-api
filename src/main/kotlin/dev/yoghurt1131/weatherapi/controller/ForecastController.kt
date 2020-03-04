c7c6970a0cf6a61c415976ee7b71676b847c4048
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-03-04 22:50:54 +0900

    receive parameter @/forecast/daily

--- src/main/kotlin/dev/yoghurt1131/weatherapi/controller/ForecastController.kt
+++ src/main/kotlin/dev/yoghurt1131/weatherapi/controller/ForecastController.kt
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController
 class ForecastController(private val weatherApiService: WeatherApiService) {
 
     @GetMapping("/daily")
-    fun dailyForecast() = weatherApiService.getTodaysWeather("Tokyo");
+    fun dailyForecast(cityName: String) = weatherApiService.getTodaysWeather(cityName);
 
 }
