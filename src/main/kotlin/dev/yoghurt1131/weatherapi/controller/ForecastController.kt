4c0d5298d0de7cfde8c119238d9fe9cf861f8808
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-03-04 22:49:32 +0900

    move forecast controller

---
+++ src/main/kotlin/dev/yoghurt1131/weatherapi/controller/ForecastController.kt
@@ -0,0 +1,15 @@
+package dev.yoghurt1131.weatherapi.controller
+
+import dev.yoghurt1131.weatherapi.application.service.WeatherApiService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/forecast")
+class ForecastController(private val weatherApiService: WeatherApiService) {
+
+    @GetMapping("/daily")
+    fun dailyForecast() = weatherApiService.getTodaysWeather("Tokyo");
+
+}
