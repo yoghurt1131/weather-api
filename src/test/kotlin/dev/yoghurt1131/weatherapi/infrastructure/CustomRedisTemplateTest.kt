2403b62f278d4a804ccef0e5561b982c65ce04c2
IshinFUKUOKA <yoghurt1131@gmail.com>
2020-04-05 15:46:49 +0900

    add CustomRedisTemplate NG cases

--- src/test/kotlin/dev/yoghurt1131/weatherapi/infrastructure/CustomRedisTemplateTest.kt
+++ src/test/kotlin/dev/yoghurt1131/weatherapi/infrastructure/CustomRedisTemplateTest.kt
@@ -3,7 +3,6 @@ package dev.yoghurt1131.weatherapi.infrastructure
 import dev.yoghurt1131.weatherapi.domain.City
 import dev.yoghurt1131.weatherapi.domain.Temperature
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
 import io.mockk.spyk
 import org.mockito.MockitoAnnotations
@@ -12,6 +11,7 @@ import org.spekframework.spek2.style.specification.describe
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.ValueOperations
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 
 object CustomRedisTemplateTest: Spek( {
@@ -39,6 +39,24 @@ object CustomRedisTemplateTest: Spek( {
             val actual = target.read(cityName)
 
             assertEquals(city, actual);
+        }
+
+        it("returns null when value is not in Cache.") {
+            every { valueOperations.get(cityName) } returns null
+            every { target.opsForValue() } returns valueOperations
+
+            val actual = target.read(cityName)
+
+            assertNull(actual)
+        }
+
+        it("returns null when exception has occurred") {
+            every { valueOperations.get(cityName) } throws RuntimeException("DUMMY")
+            every { target.opsForValue() } returns valueOperations
+
+            val actual = target.read(cityName)
+
+            assertNull(actual)
         }
     }
 
