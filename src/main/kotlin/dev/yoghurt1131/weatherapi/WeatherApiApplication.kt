af0cec66012dd3debd372a26b1f671d8def29b68
yoghurt1131 <yoghurt1131@gmail.com>
2020-06-16 20:38:29 +0900

    remove redundant

--- src/main/kotlin/dev/yoghurt1131/weatherapi/WeatherApiApplication.kt
+++ src/main/kotlin/dev/yoghurt1131/weatherapi/WeatherApiApplication.kt
@@ -1,11 +1,9 @@
 package dev.yoghurt1131.weatherapi
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableAutoConfiguration
 class WeatherApiApplication
 
 fun main(args: Array<String>) {
